91750c60cc447f19f0077c2beee070660a6278e4
Josh Fisher <jfisherd@gmail.com>
2025-11-05 16:12:10 -0500

    event attached to submit button, username.validity behaving unexpectedly

---
+++ script.js
@@ -0,0 +1,28 @@
+username = document.getElementById("username")
+usernameError = document.getElementById("usernameError")
+email = document.getElementById("email")
+emailError = document.getElementById("emailError")
+password = document.getElementById("password")
+passwordError = document.getElementById("passwordError")
+confirmPassword = document.getElementById("confirmPassword")
+confirmPasswordError = document.getElementById("confirmPasswordError")
+submitButton = document.getElementById("submitButton")
+
+// username.addEventListener('input',(event)=>{
+//     if(username.validity.tooShort) {
+//         usernameError.textContent = "Username cannot be blank"
+//     }
+// })
+
+
+submitButton.addEventListener('click',(event)=>{
+    alert('button clicked')
+    event.preventDefault()
+    alert('default prevented')
+    if(username.validity.tooShort) {
+        alert('if is TRUE')
+        usernameError.textContent = "Username cannot be blank"
+        return
+    }
+    alert('past if statement')
+})
